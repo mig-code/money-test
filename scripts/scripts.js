8d8cea299b93abd221a73efcf771d63a60fc88fe
Miguel <99726342+mig-code@users.noreply.github.com>
2022-02-16 17:27:20 +0100

    Adding Currency Selector
    Adding a currency selector a fix Infinity months error

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,45 +6,58 @@ let gasto=1500;
 let meses;
 let sueldo=1800;
 let MesesRendodeados;
+let currencySelector_el =document.getElementById("currencySelector").value
+let currencyInUse =currencySelector_el;
 
         
 function updateTextInput(val) {
-          document.getElementById('textInput').textContent=val+ " \u20ac"; 
+          document.getElementById('textInput').textContent=val+ currencyInUse; 
           ahorro=val;
-          Calcmeses (ahorro,gasto);
+          Calcmeses ();
         
           
         }
 
 function updateTextInput2(val) {
-          document.getElementById('textInput2').textContent=val+ " \u20ac"; 
+          document.getElementById('textInput2').textContent=val+ currencyInUse; 
           gasto= val;
-          Calcmeses (ahorro,gasto);
+          Calcmeses ();
           
           
         }
 function updateTextInput3(val) {
-          document.getElementById('textInput3').textContent= val+ " \u20ac"; 
+          document.getElementById('textInput3').textContent= val+ currencyInUse; 
           sueldo= val;
-          Calcmeses (ahorro,gasto);
+          Calcmeses ();
           
           
           
         }
 
-function Calcmeses (ahorro,gasto){
-            var meses = (ahorro) / (gasto - sueldo);
+function Calcmeses (){
+            meses = (ahorro) / (gasto - sueldo);
             MesesRendodeados = Math.trunc(meses)
 
-            if(gasto===sueldo){
+            if(gasto==sueldo){
+              var meses = (ahorro) / (gasto - sueldo);
+                   MesesRendodeados = Math.trunc(meses)
                 document.getElementById('resultado').innerHTML = "Result: You earn exactly enough to cover your expenses";
-            }else if(MesesRendodeados>0){
+            }else if(gasto>sueldo){
                 document.getElementById('resultado').innerHTML = "Result: "+ MesesRendodeados +" months left";
             }
             else{
                 document.getElementById('resultado').innerHTML = "Result: You earn enough to cover your expenses";
             }
-         }         
+         }    
+         
+function updateCurrency(){
+
+  currencyInUse=document.getElementById("currencySelector").value
+  updateTextInput(ahorro)
+  updateTextInput2(gasto)
+  updateTextInput3(sueldo)
+
+}
 
             
             
